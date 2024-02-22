8e5e1562d024ff3caf2c5f000d74377780257697
Andreas Weber <andy@josoansi.de>
2024-02-22 09:49:59 +0100

    extend hardware test to drive both steppers and pen servo

--- firmware/README.md
+++ firmware/README.md
@@ -1,4 +1,8 @@
 ## Arduino IDE projects here
 
-* hardware_test: simple programm to check the eggbot77 hardware (stepper and servo) without PC
+* hardware_test: simple programm to check the eggbot77 hardware (stepper and servo) without PC. Attention: The stepper motors make several fast revolutions. The program should therefore only be executed as long as the servo and stepper motors have not yet been installed.
 * EggBott77: firmware which interprets the EBB command set send from the inkscape extension
+
+# Arduino IDE settings common for all projects here
+
+* USB CDC On Boot: "Enabled"
--- firmware/hardware_test/hardware_test.ino
+++ firmware/hardware_test/hardware_test.ino
@@ -1,61 +1,132 @@
+/*
+ * Simple hardware test for the EggBot77 version 25.01.2024
+ * Servo sweeps between 850µs and 2150 and both steppers
+ * run in velocity mode with step frequency between 440Hz and 7040 Hz.
+ *
+ * Attention: The stepper motors make several fast revolutions.
+ * The program should therefore only be executed as long as the servo
+ * and stepper motors have not yet been installed.
+ */
+
 #include <driver/ledc.h>
 
-#define STEPPER_EN 1
+#define STEPPER_EN 1  // for both steppers
 #define DIR1 2
 #define STEP1 3
 #define DIR2 4
 #define STEP2 5
+#define LED_BUILTIN 15  // the blue onboard LED
+
 #define SERVO_PWM 6
-#define LED_BUILTIN 15  // die blaue onboard LED
 
-void setup() {
-  Serial.begin(115200);
+// ESP32 LED Controller (we use them here for generating servo PWM and stepper frequencies)
+#define SERVO_LEDC_CH 1
+#define STEP1_LEDC_CH 2
+#define STEP2_LEDC_CH 3
 
-  pinMode(STEPPER_EN, OUTPUT);
-  pinMode(DIR1, OUTPUT);
-  pinMode(STEP1, OUTPUT);
-  pinMode(DIR2, OUTPUT);
-  pinMode(STEP2, OUTPUT);
-  pinMode(SERVO_PWM, OUTPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
+#define FREQ_HIGH 7040
+#define FREQ_LOW 220
 
-  // enable dauerhaft setzen (active low)
-  digitalWrite(STEPPER_EN, LOW);
+hw_timer_t *Timer0_Cfg = NULL;
 
-  digitalWrite(DIR1, LOW);
-  digitalWrite(DIR2, LOW);
-
-  Serial.println("Starting");
-  Serial.setDebugOutput(true);
-
-  ledcAttachPin(LED_BUILTIN, 1);
-  ledcSetup(1, 50, 10);
+void set_freq(uint8_t ch, int16_t freq) {
+  ledcSetup(ch, freq, 10);  // 14bit
+  ledcWrite(ch, (1 << 9));  // 50% duty cycle
 }
 
-#define toggle(pin) digitalWrite(pin, !digitalRead(pin))
+// 200Hz/5ms task
+void IRAM_ATTR Timer0_ISR() {
+  static uint16_t cnt = 0;
+
+  // toggle blue onboard LED every 500ms
+  if (cnt % 100 == 0)
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+
+  // sweep servo
+  {
+    const int16_t min_width = 850;   // [µs]
+    const int16_t max_width = 2150;  // [µs]
+    static int8_t pwm_inc = 1;       // ramp slope, µs width per 10ms
+
+    static int16_t pwm_width = min_width;
+    pwm_width += pwm_inc;
+    if (pwm_width > max_width) {
+      pwm_width = max_width;
+      pwm_inc *= -1;
+    } else if (pwm_width < min_width) {
+      pwm_width = min_width;
+      pwm_inc *= -1;
+    }
+    int16_t pwm_val = pwm_width * 4096 / 20000;
+    ledcWrite(SERVO_LEDC_CH, pwm_val);
+  }
+
+  // both servos in velocity mode
+  // change frequency every second
+  if (cnt % 200 == 0) {
+    static int16_t freq = FREQ_LOW;
+    static int8_t freq_dir = 1;  // 0 = down, 1 = up
+
+    if (freq_dir == 1) {
+      freq = freq * 2;
+      if (freq >= FREQ_HIGH) {
+        freq_dir = 0;
+      }
+    } else if (freq_dir == 0) {
+      freq = freq / 2;
+      if (freq <= FREQ_LOW) {
+        freq = FREQ_LOW;
+        freq_dir = 1;
+        digitalWrite(DIR1, !digitalRead(DIR1));
+        digitalWrite(DIR2, !digitalRead(DIR2));
+      }
+    }
+    Serial.printf("Setting stepper frequency to %i Hz...\n", freq);
+    set_freq(STEP1_LEDC_CH, freq);
+    set_freq(STEP2_LEDC_CH, freq);
+  }
+  cnt++;
+}
+
+void setup() {
+  Serial.begin(9600);
+  Serial.setDebugOutput(true);
+
+  // delay 2000ms so the Serial Monitor can reconnect to USB CDC after flashing
+  delay(2000);
+  Serial.println("This is " __FILE__ ", compiled at " __DATE__ ", " __TIME__);
+
+  // enable both steppers permanently (active low)
+  pinMode(STEPPER_EN, OUTPUT);
+  digitalWrite(STEPPER_EN, LOW);
+
+  pinMode(DIR1, OUTPUT);
+  digitalWrite(DIR1, LOW);
+  pinMode(DIR2, OUTPUT);
+  digitalWrite(DIR2, LOW);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  // attach Servo PWM pin to channel 2
+  ledcAttachPin(SERVO_PWM, SERVO_LEDC_CH);
+  ledcSetup(SERVO_LEDC_CH, 50, 12);
+
+  // attach both STEP pins for the steppers
+  ledcAttachPin(STEP1, STEP1_LEDC_CH);
+  ledcAttachPin(STEP2, STEP2_LEDC_CH);
+  set_freq(STEP1_LEDC_CH, FREQ_LOW);
+  set_freq(STEP2_LEDC_CH, FREQ_LOW);
+
+  // configure 5ms timer
+  Timer0_Cfg = timerBegin(0, 80, true);
+  timerAttachInterrupt(Timer0_Cfg, &Timer0_ISR, true);
+  timerAlarmWrite(Timer0_Cfg, 5000, true);  // 80MHz/80/5000 -> 5ms task
+
+  // enable it
+  timerAlarmEnable(Timer0_Cfg);
+  Serial.println("leaving setup()...");
+}
 
 void loop() {
-  toggle(STEP1);
-  toggle(STEP2);
-  delay (1);
-
-  // servo
-  static int8_t d = 1;
-  static int16_t val = 0;
-  if (val == 1023)
-  {
-    d = -1;
-    digitalWrite(DIR1, LOW);
-    digitalWrite(DIR2, LOW);
-  }
-  else if (val == 0)
-  {
-    d = 1;
-    digitalWrite(DIR1, HIGH);
-    digitalWrite(DIR2, HIGH);
-  }
-  val += d;
-  ledcWrite(1, val);
-
-  //Serial.write("hello world\n");
+  // nothing here, all work is done in timer interrupt
 }
